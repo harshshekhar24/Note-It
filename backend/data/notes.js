af67eef9252382c0dd9adfe7b4976e83e35a8597
unknown <harshshekhar24@gmail.com>
2024-03-31 23:42:57 +0530

    Initializing backend and frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,16 @@
+const notes = [
+    {
+        _id: "1",
+        title: "Practice",
+        content: "Data loading content successfully",
+        category: "Data"
+    },
+    {
+        _id: "2",
+        title: "Testing",
+        content: "Testing successfully",
+        category: "Test"
+    }
+]
+
+module.exports = notes;
